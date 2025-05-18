a635745b97764559ca1cd40b655c902e1140c8a3
tz2015-tatiana <tz2015@hw.ac.uk>
2025-05-18 20:07:37 +0400

    update blogicum app urls and add views.py

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -1,35 +1,34 @@
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import include, path, reverse_lazy
-from django.contrib.auth import logout
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
-from django.views.generic.edit import CreateView
+from django.urls import include, path
+
+from .views import UserRegisterView
 
 
-handler404 = 'core.views.page_not_found'
-handler500 = 'core.views.server_error'
+handler404 = "core.views.page_not_found"
+handler500 = "core.views.server_error"
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('blog.urls', namespace='blog')),
-    path('pages/', include('pages.urls', namespace="pages")),
-    path('auth/', include('django.contrib.auth.urls')),
     path(
-        'auth/logout/',
-        lambda request: (
-            logout(request),
-            render(request, 'registration/logged_out.html'))[1],
-        name='logout',
+        "admin/",
+        admin.site.urls
     ),
     path(
-        'auth/registration/',
-        CreateView.as_view(
-            template_name='registration/registration_form.html',
-            form_class=UserCreationForm,
-            success_url=reverse_lazy('login'),
-        ),
-        name='registration',
+        "",
+        include("blog.urls", namespace="blog")
+    ),
+    path(
+        "pages/",
+        include("pages.urls", namespace="pages")
+    ),
+    path(
+        "auth/",
+        include("django.contrib.auth.urls")
+    ),
+    path(
+        "auth/registration/",
+        UserRegisterView.as_view(),
+        name="registration"
     ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
