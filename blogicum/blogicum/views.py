2ff7e71ed65bbb0150739859f039a4dc9b79b3a0
tz2015-tatiana <tz2015@hw.ac.uk>
2025-05-19 15:59:00 +0400

    Удален файл из отслеживания

--- blogicum/blogicum/views.py
+++
@@ -1,9 +0,0 @@
-from django.urls import reverse_lazy
-from django.views.generic import CreateView
-from django.contrib.auth.forms import UserCreationForm
-
-
-class UserRegisterView(CreateView):
-    template_name = "registration/registration_form.html"
-    form_class = UserCreationForm
-    success_url = reverse_lazy("login")
